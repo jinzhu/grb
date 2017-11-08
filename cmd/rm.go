5f6d19049f856c912c133f6181f829e16235d4f0
Jinzhu <wosmvp@gmail.com>
2017-11-08 19:30:27 +0800

    Add rm

---
+++ cmd/rm.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.AddCommand(&cobra.Command{
+		Use:   "rm [branch]",
+		Short: "delete branch `branch`",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			values := map[string]string{"branch": args[0]}
+
+			sh("{{git}} push {{origin}} :refs/heads/{{branch}}", values)
+			if getCurrentBranch() == args[0] {
+				sh("{{git}} checkout master")
+			}
+			sh("{{git}} branch -d {{branch}}", values)
+		},
+	})
+}
